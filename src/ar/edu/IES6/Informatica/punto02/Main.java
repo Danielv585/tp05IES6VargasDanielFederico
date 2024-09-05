6a31e2ec290f5adfac30c618cfea1588a161af90
Daniel V <Daniel V@10.0.5.109>
2024-09-04 22:29:45 -0300

    cambios en el punto 02 VARGAS

--- src/ar/edu/IES6/Informatica/punto02/Main.java
+++ src/ar/edu/IES6/Informatica/punto02/Main.java
@@ -1,20 +1,31 @@
 package ar.edu.IES6.Informatica.punto02;
+import java.util.InputMismatchException;//tratamiento de Error
 import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner stdln=new Scanner(System.in);
 		int num;
-		System.out.print("Ingrese Un Numero Entero : ");
-		num=stdln.nextInt();
-		if (num % 2==0){
-			System.out.println("Es Par "+num);
-			System.out.println("El Triple del Numero es : "+num*3);
-		}else {
-			System.out.println("Es Impar"+num);
-			System.out.println("El doble del Numero es : "+num*2);
-		}
-		
+		boolean continua;
+		do {
+			try {
+				continua=false;
+				System.out.print("Ingrese Un Numero Entero : ");
+				num=stdln.nextInt();
+				if (num % 2==0){
+					System.out.println("Es Par "+num);
+					System.out.println("El Triple del Numero es : "+num*3);
+				}else {
+					System.out.println("Es Impar"+num);
+					System.out.println("El doble del Numero es : "+num*2);
+				}
+			}catch(InputMismatchException ex){
+				stdln.next();
+				continua=true;
+			}
+			
+		}while(continua);
+			
 	}
 
 }
